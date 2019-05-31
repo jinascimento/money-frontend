5976fca1f7f8c0e3f727b1749952966b8e97d3f4
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-05-30 23:04:25 -0300

    Routes

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className={'main-footer'}>
+        <strong>
+            Copyright &copy; 2019
+            <a href={'https://github.com/jinascimento'} target={'_blank'}>Jhonatan I. Nascimento</a>
+        </strong>
+    </footer>
+)
