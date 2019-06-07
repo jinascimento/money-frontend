9a19bdcba50006a523538fba76a65b587f61b00d
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-06-06 21:27:02 -0300

    Update BillingCycle

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from "redux-form";
 
+import labelAndInput from '../common/form/labelAndInput'
 class BillingCycleForm extends Component {
     render(){
         const { handleSubmit } = this.props
@@ -9,16 +10,19 @@ class BillingCycleForm extends Component {
         return(
             <form role={'form'} onSubmit={handleSubmit}>
                 <div className={'box-body'}>
-                    <Field name={'name'} component={'input'} />
-                    <Field name={'month'} component={'input'} />
-                    <Field name={'year'} component={'input'} />
+                    <Field name={'name'} component={labelAndInput}
+                    label={'Nome'} cols={'12 4'} placeholder={'Informe o nome'} />
+                    <Field name={'month'} component={labelAndInput} label={'Mês'}
+                    cols={'12 4'} placeholder={'Informe o mês'}/>
+                    <Field name={'year'} component={labelAndInput} label={'Ano'}
+                    cols={'12 4'} placeholder={'Informe o ano'} />
                 </div>
                 <div className="box-footer">
-                    <button type={'submit'} className={'btn btn-primary'}>Submit</button>
+                    <button type={'submit'} className={'btn btn-primary'}>Salvar</button>
                 </div>
             </form>
         )
     }
 }
 
-export default reduxForm({form: 'billingCycleForm'})(BillingCycleForm)
+export default reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
