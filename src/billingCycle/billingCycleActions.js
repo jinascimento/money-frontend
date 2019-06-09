91f952a0f30d67f957ad58758c607a3a4d30ce30
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-06-09 19:43:52 -0300

    Configs to heroku

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -3,7 +3,7 @@ import { toastr } from "react-redux-toastr";
 import { reset as resetForm, initialize } from "redux-form";
 import { showTabs, selectTab } from "../common/tab/tabActions";
 
-const BASE_URL = 'http://localhost:3003/api'
+const BASE_URL = 'https://my-money-app-backend-jhony.herokuapp.com/api'
 const INITIAL_VALUES = {credits: [{}], debits: [{}]}
 
 export function getList(){
